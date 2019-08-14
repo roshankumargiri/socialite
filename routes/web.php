11d1f4297a678bf6c5714be8d5c2212d76efb16a
roshankumargiri <roshankumar.giri19@gmail.com>
2019-08-14 23:15:02 +0545

    facebook login

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/login/{provider}', 'Auth\SocialAccountController@redirectToProvider');
+Route::get('/login/{provider}/callback', 'Auth\SocialAccountController@handleProviderCallback');
